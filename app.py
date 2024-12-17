d6e11c15e730d408e8f6e72bd44585a297645f12
monicaluo-nyush <ml8110@nyu.edu>
2024-12-16 19:13:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def load_data():
 
 X, y = load_data()
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y)
 #rfc
 rf_clf = RandomForestClassifier(n_estimators=100, max_depth=None, min_samples_split=2, min_samples_leaf=1, max_features='sqrt')
 rf_clf.fit(X_train, y_train)
